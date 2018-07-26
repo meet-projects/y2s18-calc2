48f1912c2d7ed2d3ee0250be2dd4ccfa4b372dca
Matthew Feng <mattfeng@mit.edu>
2018-07-26 05:10:54 +0300

    Convert list to table

---
+++ README.md
@@ -0,0 +1,18 @@
+# Lab: `calc2`
+<table>
+  <tr>
+    <th>Lecture</th>
+    <td>Day 3 Lecture 2: <a href="">Introduction to JavaScript, Part 2</a></td>
+  </tr>
+  <tr>
+    <th>Description</th>
+    <td>In this lab, we will build on the functions that we have written in the previous lab, `calc1`, and create a six-function calculator web application with a front-end user interface that the user can click and interact with.</td>
+  </tr>
+</table>
+
+## 0 &ensp; Notes about the Starter Code
+1. We have copied the functions (`exponent`, `calculate`) you have already written from lab `calc1` into `calc.js` as starter code; you should not need to modify these functions to complete this lab.
+2. We have added one style into the CSS for you. Feel free to edit any and all styles in `calc.css` as you see fit.
+3. If you have any questions, please ask!
+
+##
--- lab-files/js/calc.js
+++ lab-files/js/calc.js
@@ -1,1 +1,40 @@
-// your calculator code goes here - you got this!
+/* START OF STARTER CODE: You should not need to modify these functions. */
+function exponent(base, power) {
+  var ans = 1;
+
+  for (var i = 0; i < power; i++) {
+    ans *= base;
+  }
+
+  return ans;
+}
+
+function calculate(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+      // Notice that we don't have break statements because
+      // return will already break for us.
+
+    case '-':
+      return value1 - value2;
+
+    case '*':
+      return value1 * value2;
+
+    case '/':
+      return value1 / value2;
+
+    case '%':
+      return value1 % value2;
+
+    case '^':
+      return exponent(value1, value2);
+    
+    default:
+      return false;
+  }
+}
+/* END OF STARTER CODE */
+
+// Your code goes here -- good luck!
